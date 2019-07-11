72581936026269701a370d6dca83e461304ef931
xavier <xavier_22_mora@hotmail.com>
2019-07-11 11:21:21 +0200

    ajout fonts plus img,  frontend fini

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
-<html   {{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Marketing Lab</title>
-            <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> 
+<html {{ str_replace('_', '-', app()->getLocale()) }}">
 
-            <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"/>
-            <link rel="stylesheet" href="{{asset('css/plugin.css')}}"/>
-            <link rel="stylesheet" href="{{asset('css/style.css')}}"/>
-            <link rel="stylesheet" href="{{asset('css/responsive.css')}}"/>
-            <script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
-    </head>
-    <body>
-        <div>
-            @include('layouts.nav')
-            @yield('content')
-            
-        </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Marketing Lab</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/plugin.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/responsive.css')}}" />
+    <script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
+</head>
+
+<body>
+    <div>
+        @include('layouts.nav')
+        @yield('content')
+
+    </div>
     <script type="text/javascript" src="{{asset('js/plugin.js')}}"></script>
     <script type="text/javascript" src="{{asset('js/main.js')}}"></script>
     <script type="text/javascript">
@@ -35,4 +37,4 @@
 
         });
     </script>
-    </body>
+</body>
